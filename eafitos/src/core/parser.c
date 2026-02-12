1ff06fd5e1c2334a96923bc680ffe0fd37c435bb
NReyesC <nicoreyes.c7@gmail.com>
2026-02-12 17:52:39 +0000

    se creo el parser que convierte la linea de entrada de la shell en argumentos listos para ejecutar comandos

--- eafitos/src/core/parser.c
+++ eafitos/src/core/parser.c
@@ -1,26 +1,64 @@
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
+#include "parser.h"
 #include "utils.h"
 
-#define MAX_ARGS 64
-#define DELIM " \t\r\n\a"
+// Convierte una linea en un arreglo de tokens segun delimitadores del parser.
+ParserResult parser_parse_line(char *linea) {
+    ParserResult res;
+    res.args = NULL;
+    res.argc = 0;
+    res.status = PARSER_OK;
 
-// Tokeniza la linea de entrada en argumentos para el ejecutor de comandos.
-char **parsear_linea(char *linea) {
-    int bufsize = MAX_ARGS;
+    if (linea == NULL || linea[0] == '\0') {
+        res.status = PARSER_ERR_EMPTY;
+        return res;
+    }
+
+    char **tokens = mm_malloc(PARSER_MAX_ARGS * sizeof(char *));
+    if (tokens == NULL) {
+        res.status = PARSER_ERR_OOM;
+        return res;
+    }
+
     int posicion = 0;
-    char **tokens = mm_malloc(bufsize * sizeof(char*));
-    char *token;
-
-    // Segmenta usando delimitadores de espacio y control.
-    token = strtok(linea, DELIM);
-    while (token != NULL) {
+    char *token = strtok(linea, PARSER_DELIM);
+    while (token != NULL && posicion < (PARSER_MAX_ARGS - 1)) {
         tokens[posicion] = token;
         posicion++;
-
-        token = strtok(NULL, DELIM);
+        token = strtok(NULL, PARSER_DELIM);
     }
-    tokens[posicion] = NULL; // El último debe ser NULL para funciones como execvp
-    return tokens;
+    // El ultimo debe ser NULL para funciones como execvp.
+    tokens[posicion] = NULL;
+
+    res.args = tokens;
+    res.argc = posicion;
+    res.status = (posicion == 0) ? PARSER_ERR_EMPTY : PARSER_OK;
+    return res;
+}
+
+// Libera el arreglo de argumentos generado por el parser.
+void parser_free_result(ParserResult *res) {
+    if (res == NULL || res->args == NULL) {
+        return;
+    }
+    mm_free(res->args);
+    res->args = NULL;
+    res->argc = 0;
+    res->status = PARSER_OK;
+}
+
+// Indica si el resultado no tiene comandos para ejecutar.
+int parser_is_empty(const ParserResult *res) {
+    if (res == NULL || res->args == NULL) {
+        return 1;
+    }
+    return (res->argc == 0 || res->args[0] == NULL) ? 1 : 0;
+}
+
+// Compatibilidad: devuelve solo el arreglo de argumentos.
+char **parsear_linea(char *linea) {
+    ParserResult res = parser_parse_line(linea);
+    return res.args;
 }
